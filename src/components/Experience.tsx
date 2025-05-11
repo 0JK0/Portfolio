321632cd3d082c004e0fe62ead6fd4d5a5eb5f19
0JK0 <chkiller96@gmail.com>
2025-05-11 02:20:26 -0500

    Finished Timeline, i think

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -12,21 +12,42 @@ export function Timeline(){
 
                 <ul className='timeLine-list'>
                     <li className='timeLine-point'>
-                        <span className='timeLine-jobName'>Tutor De Programación</span>
-                        <span className='timeLine-date'>Febrero 2025 - Presente</span>
 
-                        <p className='timeLine-text'>Lorem Ipsum, sigmaboy, trowhs gyatt, no pole dont toliver</p> 
+                        <div className='timeLine-desc' >
+                        
+                            <span className='timeLine-jobName'>Tutor De Programación</span>
+                            <span className='timeLine-date'>Febrero 2025 - Presente</span>
+
+                        </div>
+
+                        <p className='timeLine-text'>
+                            
+                            <span><span className="purple-arrow">➤</span> Tutorías particulares en C++ y Python a compañeros universitarios.</span>                          
+                            <span><span className="purple-arrow">➤</span> Apoyo en lógica de programación, solución de ejercicios y preparación para exámenes</span>                          
+                        </p> 
+
                     </li>
+
                     <li className='timeLine-point'>
-                        <span className='timeLine-jobName'>Freelance Java</span>
-                        <span className='timeLine-date'>Junio 2021 - Marzo 2022</span>
 
-                        <p className='timeLine-text'> 
-                            25+ proyectos para clientes internacionales <br/> <br/>  
-                            Mods personalizados para Minecraft(Java), soluciones académicas y aplicaciones <br/> <br/>
-                            Calificación promedio de 4.3 estrellas. <br/> <br/>
-                        </p>
+                        <div className='timeLine-desc'>
+
+                            <span className='timeLine-jobName'>Freelance Java</span>
+                            <span className='timeLine-date'>Mayo 2021 - Marzo 2022</span>
+
+                        </div>
+
+
+
+                        <div className="timeLine-text">
+                            <span><span className="purple-arrow">➤</span> 25+ proyectos para clientes internacionales</span>
+                            <span><span className="purple-arrow">➤</span> Mods personalizados para Minecraft(Java), soluciones académicas y aplicaciones</span>
+                            <span><span className="purple-arrow">➤</span> Calificación promedio de 4.3 estrellas.</span>
+                        </div>
+
+
                     </li>
+
                 </ul>
 
             </div>
